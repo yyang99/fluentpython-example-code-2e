5a455d9f3b716aa487dfaf117d465af49d121b2d
yyang99 <yyang99@hotmail.com>
2024-03-02 11:22:06 -0500

    stacked_decorator_vs_decorator_param_run_import_time.py

---
+++ 09-closure-deco/stacked_decorator_vs_decorator_param_run_import_time.py
@@ -0,0 +1,55 @@
+def first(f):
+    print(f'apply first({f.__name__})')
+
+    def inner1st(n):
+        result = f(n)
+        print(f'inner1({n}): called {f.__name__}({n}) -> {result}')
+        return result
+    return inner1st
+
+
+def second(f):
+    print(f'apply second({f.__name__})')
+
+    def inner2nd(n):
+        result = f(n)
+        print(f'inner2({n}): called {f.__name__}({n}) -> {result}')
+        return result
+    return inner2nd
+
+def register(key):  # <2>
+    def decorate(func):  # <3>
+        print(f'running register {func}(key={key})->decorate({func})')
+        return func  # <6>
+    return decorate  # <7>
+
+def param_decorator(parameter):
+    print(f'apply param_decorator({parameter})')
+    def decorator(func):
+        print(f'apply decorator({parameter})')
+        def inner_decorator(n):
+            result = func(n)
+            print(f'inner_decorator({n}): called {func.__name__}({n}) -> {result}')
+            return result
+
+        return decorator
+    return decorator
+
+print("\n==================[ decorator with parameter ]")
+
+@first
+@second
+def double(n):
+    return n * 2
+
+print("\n==================[ decorator with parameter ]")
+
+@param_decorator("test parameter decorator")
+def tripple(n):
+    return n * 3
+
+print("\n==================[ registration decorator with param ]")
+
+@register("key1")
+def quad(n):
+    return n * 4
